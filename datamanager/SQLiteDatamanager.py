91c0e0436862aa0e70e9be8e0214954c3dd24474
InesZU <inesrahrah@gmail.com>
2024-10-09 13:59:34 +0200

    add SQL file

--- datamanager/SQLiteDatamanager.py
+++ datamanager/SQLiteDatamanager.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, joinedload
-from .Models import Base, User, Movie, Review
+from .models import Base, User, Movie, Review
 from .DataManager import DataManagerInterface
 
 
